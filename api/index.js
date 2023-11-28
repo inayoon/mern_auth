1b56fd06dffef20871e5504ce0af5ea4d717ede0
Ina Yoon <yia0319@gmail.com>
2023-11-27 21:08:56 -0500

    Connect to mongodb

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_KEY)
+  .then(() => {
+    console.log("Conneted to the DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 app.listen(3000, () => {
