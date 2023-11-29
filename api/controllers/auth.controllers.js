214f7398d655a59b8a7fd02ed8f66fa0ec7cc5fe
Ina Yoon <yia0319@gmail.com>
2023-11-28 22:30:10 -0500

    Create middleware and functions to handle errors

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   //req -> data we are getting from the client side
   //res -> data we send back to the client side
   const { username, email, password } = req.body;
@@ -10,7 +11,7 @@ export const signup = async (req, res) => {
   try {
     await newUser.save();
     res.status(201).json({ message: "User created successfully." });
-  } catch (err) {
-    res.status(500).json(err.message);
+  } catch (error) {
+    next(error);
   }
 };
