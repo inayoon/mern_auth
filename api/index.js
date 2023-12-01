d441116f5b115e067d8d29851227ef11a9040daf
Ina Yoon <yia0319@gmail.com>
2023-12-01 16:11:28 -0500

    Add pre production changes

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 dotenv.config();
@@ -15,7 +16,13 @@ mongoose
     console.log(err);
   });
 
+const __dirname = path.resolve();
 const app = express();
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 app.use(express.json());
 app.use(cookieParser());
 app.listen(3000, () => {
